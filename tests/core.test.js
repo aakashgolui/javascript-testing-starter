46528bd2c1a6260d83cc7af0d6615e7a4d54a41c
aakashgolui <iamakashgolui140@gmail.com>
2025-05-19 11:53:59 +0530

    feat:added some more tests

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,4 +1,10 @@
-import { calculateDiscount, getCoupons, validateUserInput } from "../src/core";
+import {
+  calculateDiscount,
+  getCoupons,
+  isPriceInRange,
+  isValidUsername,
+  validateUserInput,
+} from "../src/core";
 import { describe, it, expect } from "vitest";
 
 describe("getCoupons", () => {
@@ -69,5 +75,56 @@ describe("validateUserInput", () => {
   it("should check invalid age", () => {
     expect(validateUserInput("akash", "27")).toMatch(/invalid age/i);
     expect(validateUserInput("akash", 17)).toMatch(/invalid age/i);
+  });
+
+  it("should return an error if username is longer than 255 characters", () => {
+    expect(validateUserInput("A".repeat(256), 27)).toMatch(/invalid/i);
+  });
+
+  it("should return an error if age is greater than 100", () => {
+    expect(validateUserInput("akash", 101)).toMatch(/invalid/i);
+  });
+
+  it("should return an error if both are invalid", () => {
+    expect(validateUserInput("", 0)).toMatch(/invalid username/i);
+    expect(validateUserInput("", 0)).toMatch(/invalid age/i);
+  });
+});
+
+describe("isPriceInRange", () => {
+  it("should return false if price is outside of range", () => {
+    expect(isPriceInRange(-10, 0, 100)).toBeFalsy();
+    expect(isPriceInRange(200, 0, 100)).toBeFalsy();
+  });
+
+  it("should return true if price is equal to min or max", () => {
+    expect(isPriceInRange(0, 0, 100)).toBe(true);
+    expect(isPriceInRange(100, 0, 100)).toBe(true);
+  });
+
+  it("should return true if price is within the range", () => {
+    expect(isPriceInRange(20, 0, 100)).toBe(true);
+  });
+});
+
+describe("isValidUsername", () => {
+  const minLength = 5;
+  const maxLength = 15;
+  it("should return false if username length is outside of the range", () => {
+    expect(isValidUsername("a".repeat(minLength - 1))).toBe(false);
+    expect(isValidUsername("a".repeat(maxLength + 1))).toBe(false);
+  });
+
+  it("should return true if username length is equal to max and min", () => {
+    expect(isValidUsername("a".repeat(minLength))).toBe(true);
+    expect(isValidUsername("a".repeat(maxLength))).toBe(true);
+  });
+
+  it("should return true if username length is within the range", () => {
+    expect(isValidUsername("akashgolui")).toBe(true);
+  });
+
+  it("should return invalid if username is not a string", () => {
+    expect(isValidUsername(0)).toMatch(/invalid/i);
   });
 });
